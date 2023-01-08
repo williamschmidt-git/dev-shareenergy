a4957198e8e55983be8d0de3f60326772c6e7f08
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-08 19:59:00 -0300

    💄 add: login page styles

--- frontend/src/components/LoginComponent.tsx
+++ frontend/src/components/LoginComponent.tsx
@@ -1,9 +1,39 @@
 export default function LoginComponent() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-      Install & Setup Vite + React + Typescript + Tailwind CSS 3
-      </h1>
+    <div className="bg-gray-200 h-screen">
+      <div className="pt-44 mb-8">
+        <h1 className="text-center text-3xl font-bold text-gray-700">Sign in to your account</h1>
+      </div>
+      
+
+      <div className="bg-white max-w-lg rounded-lg mx-auto p-10 shadow-lg">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="username" className="text-sm text-gray-600">
+              Username:
+              <input type="text" className="block border rounded w-full py-1 px-2" />
+            </label>
+          </div>
+          
+          <div className="mb-3">
+            <label htmlFor="password" className="text-sm text-gray-600">
+                Password:
+                <input type="password" className="block border rounded w-full py-1 px-2"/>
+              </label>
+          </div>
+
+          <div className="mb-7">
+            <input type="checkbox" className="m-1 "/>
+            <label htmlFor="remember-me" className="text-base text-gray-600">
+                Remember me
+              </label>
+          </div>
+
+            <button className="bg-indigo-600 px-4 py-2 rounded-lg text-white font-medium w-1/2 mx-auto flex align-middle justify-center">
+              Sign in
+            </button>
+        </form>
+      </div>
     </div>
   )
 }
