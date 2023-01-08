16a48ca9b10967a4b01f39b7466707c14e1aa898
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-08 17:03:23 -0300

    💄 install + setup tailwind css

--- frontend/src/components/LoginComponent.tsx
+++ frontend/src/components/LoginComponent.tsx
@@ -1,5 +1,9 @@
 export default function LoginComponent() {
   return (
-    <div>Login Component</div>
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+      Install & Setup Vite + React + Typescript + Tailwind CSS 3
+      </h1>
+    </div>
   )
 }
