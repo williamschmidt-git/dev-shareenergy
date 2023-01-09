b748c59e169b923e15a38fa2c459ea9eb6506e8a
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-09 12:41:43 -0300

    ✨ feat: install passport for auth middlewares

--- backend/src/app/entities/user.ts
+++ backend/src/app/entities/user.ts
@@ -1,1 +1,38 @@
-export class User {}
+import { ObjectId } from 'bson';
+
+export interface UserData {
+  id?: string;
+  username: string;
+  password: string;
+}
+
+export class User {
+  private props: UserData;
+
+  constructor(props: UserData) {
+    this.props = {
+      ...props,
+      id: new ObjectId().toString(),
+    };
+  }
+
+  public get id(): string {
+    return this.props.id;
+  }
+
+  public set username(username: string) {
+    this.props.username = username;
+  }
+
+  public get username(): string {
+    return this.props.username;
+  }
+
+  public set password(password: string) {
+    this.props.password = password;
+  }
+
+  public get password(): string {
+    return this.props.password;
+  }
+}
