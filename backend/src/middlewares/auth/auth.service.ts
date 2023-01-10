71d5c8fe80b6a4ecddccd8520c5f0a1b1723fd3c
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-09 22:04:37 -0300

    ✨ feat: add auth service and module

---
+++ backend/src/middlewares/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/app/entities/user';
+import { FindUserByUsername } from 'src/app/use-cases/user/find-user-by-username';
+import { HashComparator } from 'src/helpers/hash-comparator';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly findUserByUsername: FindUserByUsername,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(user: User): Promise<any> {
+    const userToCompare = await this.findUserByUsername.execute({
+      username: user.username,
+    });
+
+    if (!userToCompare) {
+      throw new UnauthorizedException('Invalid user or password.');
+    }
+
+    const { password } = user;
+
+    if (await HashComparator.compare(password, userToCompare.user.password)) {
+      return await this.genToken(user);
+    }
+
+    throw new UnauthorizedException('Invalid user or password.');
+  }
+
+  async genToken(payload: User) {
+    return {
+      access_token: this.jwtService.sign(
+        {
+          username: payload.username,
+        },
+        {
+          secret: 'SECRET',
+          expiresIn: '7d',
+        },
+      ),
+    };
+  }
+}
