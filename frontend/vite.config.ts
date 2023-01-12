20dacf9113b09ba12f56976bb9944e54788e8bd5
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-11 22:52:04 -0300

    🚑 fix: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import { liveReload } from 'vite-plugin-live-reload';
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), liveReload('.path')],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
