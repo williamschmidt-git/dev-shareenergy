20f481c12880ae1dfcb7edf0f1d9a813838926f7
williamschmidt-git <9williamschmidt@gmail.com>
2023-01-15 12:46:07 -0300

    remove eslint dependencie from vite-config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
-import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
